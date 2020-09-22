4aff08244815a026e45caa2071f6bfe27ef64718
umangsaltuniv <umang@saltuniv.com>
2020-09-22 12:41:38 +0530

    CodeCommit1.0

---
+++ src/test/java/maven/selenium/SeleniumTest.java
@@ -0,0 +1,39 @@
+package maven.selenium;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import java.io.File;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+
+	@Test
+	public void test01() {
+		String driverPath = System.getProperty("user.dir")
+							+ File.separator
+							+ "drivers"
+							+ File.separator
+							+ "chromedriver.exe";
+		
+		System.setProperty("webdriver.chrome.driver", driverPath);
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		
+		String expectedTitle = "Google";
+		String actualTitle = driver.getTitle();
+		assertEquals(expectedTitle, actualTitle);
+		System.out.println(actualTitle);
+		
+		driver.quit();
+		
+		
+		
+		
+	}
+
+}
